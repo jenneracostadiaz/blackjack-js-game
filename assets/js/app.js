51619364b6d576f0ba34fd492a444dcbc25fc833
Jenner Acosta Díaz <U18312277@utp.edu.pe>
2022-07-28 02:55:25 -0500

    Creando Arreglo para neustras cartas

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+/**
+ * 2C = Two of Clubs (Tréboles)
+ * 2D = Two of Diamends (Diamantes)
+ * 2H = Two of Hearts (Corazones)
+ * 2S = Two of Spades (Espadas)
+ */
+
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
+
+const crearDeck = () => {
+  for (let i = 2; i <= 10; i++) {
+    for (let tipo of tipos) {
+      deck.push(i + tipo);
+    }
+  }
+  for (let tipo of tipos) {
+    for (let esp of especiales) {
+      deck.push(esp + tipo);
+    }
+  }
+  console.log(deck);
+  deck = _.shuffle(deck);
+  console.log(deck);
+  return deck;
+};
+
+crearDeck();
