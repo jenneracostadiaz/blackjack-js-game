7738a67f07a5e522f18258c40121eca738998214
Jenner Acosta Díaz <U18312277@utp.edu.pe>
2022-07-28 04:06:14 -0500

    Evento pedir carta

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,14 @@ let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+let puntosJugador = 0,
+  puntosComputadora = 0;
+
+//Referencias HTML
+const btnPedir = document.querySelector('#btnPedir');
+const divCartasJugador = document.querySelector('#jugardor-cartas');
+const puntosHTML = document.querySelectorAll('small');
+
 //Esta Funcion crear una nueva baraja
 const crearDeck = () => {
   for (let i = 2; i <= 10; i++) {
@@ -36,9 +44,6 @@ const pedirCarta = () => {
   }
 
   const carta = deck.pop();
-
-  console.log(deck);
-  console.log(carta); //Carta debe ser de la baraja
   return carta;
 };
 
@@ -48,5 +53,20 @@ const valorCarta = (carta) => {
   return isNaN(valor) ? (valor === 'A' ? 11 : 10) : valor * 1;
 };
 
-const valor = valorCarta(pedirCarta());
-console.log({ valor });
+//Eventos
+btnPedir.addEventListener('click', () => {
+  const carta = pedirCarta();
+  puntosJugador = puntosJugador + valorCarta(carta);
+  puntosHTML[0].innerText = puntosJugador;
+  const imgCarta = document.createElement('img');
+  imgCarta.src = `assets/cartas/${carta}.png`;
+  imgCarta.classList.add('carta');
+  divCartasJugador.append(imgCarta);
+
+  if (puntosJugador > 21) {
+    console.warn('SLo siento mucho, perdite');
+    btnPedir.disabled = true;
+  } else if (puntosJugador === 21) {
+    console.warn('21, genial!');
+  }
+});
